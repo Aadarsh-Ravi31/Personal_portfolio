fc6c209bfcb6a369e0d19815edefd23e832b02a5
Aadarsh-Ravi31 <dedlyf.casper@gmail.com>
2025-04-10 21:13:56 -0400

    updated contact details

--- portfolio/app/components/Footer.tsx
+++ portfolio/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 text-gray-300 py-4 mt-10">
+    <footer className="bg-gray-800 text-gray-300 py-4">
       <div className="container mx-auto text-center">
         <p>&copy; 2025 Aadarsh Ravi. All rights reserved.</p>
       </div>
